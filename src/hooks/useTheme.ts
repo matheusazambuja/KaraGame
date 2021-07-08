95d5dc33a9a248348243a1f425edc539e295d3b2
Matheus Azambuja <matheusazambuja35@gmail.com>
2021-07-08 16:02:09 -0300

    Created useTheme for theme

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { ThemeContext } from "../contexts/theme/ThemeContainer";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 
 export function useTheme() {
