e437d1f117c20b9414a10a7da423e5ca50d745f7
Matheus Azambuja <matheusazambuja35@gmail.com>
2021-07-08 16:02:23 -0300

    Create theme context

---
+++ src/contexts/ThemeContext.tsx
@@ -0,0 +1,39 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+import Cookies from "js-cookie"
+
+type Theme = 'light' | 'dark';
+
+type ThemeContextProps = {
+  children: ReactNode
+};
+
+type ThemeContextType = {
+  theme: Theme;
+  toggleTheme: () => void;
+};
+
+export const ThemeContext = createContext({} as ThemeContextType);
+
+export function ThemeContextProvider({ children }: ThemeContextProps) {
+  const [currentTheme, setCurrentTheme] = useState<Theme>(() => {
+    const cookiesTheme = Cookies.getJSON('theme')
+
+    return (cookiesTheme ?? 'light') as Theme;
+  });
+
+  useEffect(() => {
+    Cookies.set('theme', currentTheme);
+  }, [currentTheme]);
+
+  const toggleTheme = () => {
+    setCurrentTheme(currentTheme === 'light' ? 'dark' : 'light');
+  };
+
+  return (
+    <ThemeContext.Provider value={{
+      theme: currentTheme, toggleTheme
+    }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
