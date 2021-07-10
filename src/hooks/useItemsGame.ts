6b4c4dbd0eadefa58ceab575eab6cac8541374bf
Matheus Azambuja <matheusazambuja35@gmail.com>
2021-07-10 11:30:54 -0300

    Updated hook useItemsGame and deleted old files

--- src/hooks/useItemsGame.ts
+++ src/hooks/useItemsGame.ts
@@ -1,12 +1,8 @@
-import { useContextSelector } from 'use-context-selector';
+import { useContext } from 'react';
 import { ItemsGameContext } from '../contexts/ItemsGameContext';
 
 export function useItemsGame() {
-  const itemsInGameInformation = useContextSelector(ItemsGameContext, item => item.itemsInGameInformation);
-  const selectToggleItemGame = useContextSelector(ItemsGameContext, item => item.selectToggleItemGame);
-  const resetItemsInGame = useContextSelector(ItemsGameContext, item => item.resetItemsInGame);
+  const value = useContext(ItemsGameContext);
 
-  return {
-    itemsInGameInformation, selectToggleItemGame, resetItemsInGame
-  }
+  return value;
 }
