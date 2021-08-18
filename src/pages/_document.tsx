d09c53089904d39c59598df1eedff8b11994f21c
Matheus Azambuja <matheusazambuja35@gmail.com>
2021-08-18 10:56:59 -0300

    Add new fonts from project

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,14 +7,12 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" rel="stylesheet" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Inter&family=Rubik:wght@400;500&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
           <NextScript />
-          <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossOrigin="anonymous"
-            type="text/javascript"
-          />
         </body>
       </Html>
     );
