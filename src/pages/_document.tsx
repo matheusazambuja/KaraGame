d622656bdab11da747572ceb050a80c4f61b16d9
Matheus Azambuja <matheusazambuja35@gmail.com>
2021-08-18 23:46:31 -0300

    Fix fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,8 +6,8 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link href="https://fonts.googleapis.com/css2?family=Inter&family=Rubik:wght@400;500&display=swap" rel="stylesheet" />
         </Head>
         <body>
